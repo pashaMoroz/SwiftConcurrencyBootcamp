6759b837adb464dd496b34631fe45c9d928f2615
Moroz Pavlo <pavlo@sidekickinteractive.com>
2023-02-25 11:33:36 -0500

    Initial Commit

---
+++ SwiftConcurrenncyBootcamp/SwiftConcurrenncyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrenncyBootcampApp.swift
+//  SwiftConcurrenncyBootcamp
+//
+//  Created by Moroz Pavlo on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrenncyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
